5a0aa8e7b1175dcf1aefc74b460df8f1d8629dd4
Pavel Smirnov <smirnov@0x55.ru>
2021-03-16 20:48:23 +0300

    git start using

---
+++ src/main/java/dev/smirnoff/springbootresttemplate/SpringbootresttemplateApplication.java
@@ -0,0 +1,13 @@
+package dev.smirnoff.springbootresttemplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootresttemplateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootresttemplateApplication.class, args);
+    }
+
+}
